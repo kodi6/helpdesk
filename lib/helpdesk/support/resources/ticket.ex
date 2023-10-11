255cc49a2a7e68e07e765859661ae8f1aec66a16
kodi6 <punniyakodi@auroville.org.in>
2023-10-11 17:14:44 +0530

    Add a new `update` action and modifications to attributes

--- lib/helpdesk/support/resources/ticket.ex
+++ lib/helpdesk/support/resources/ticket.ex
@@ -5,6 +5,24 @@ defmodule Helpdesk.Support.Ticket do
   actions do
     # Add a set of simple actions. You'll customize these later.
     defaults [:create, :read, :update, :destroy]
+
+    create :open do
+      # By default you can provide all public attributes to an action
+      # This action should only accept the subject
+      accept [:subject]
+    end
+
+    update :close do
+      # We don't want to accept any input here
+      accept []
+
+      change set_attribute(:status, :closed)
+      # A custom change could be added like so:
+      #
+      # change MyCustomChange
+      # change {MyCustomChange, opt: :val}
+    end
+
   end
 
   # Attributes are the simple pieces of data that exist on your resource
@@ -13,6 +31,25 @@ defmodule Helpdesk.Support.Ticket do
     uuid_primary_key :id
 
     # Add a string type attribute called `:subject`
-    attribute :subject, :string
+    attribute :subject, :string do
+      # Don't allow `nil` values
+      allow_nil? false
+    end
+
+  # status is either `open` or `closed`. We can add more statuses later
+  attribute :status, :atom do
+    # Constraints allow you to provide extra rules for the value.
+    # The available constraints depend on the type
+    # See the documentation for each type to know what constraints are available
+    # Since atoms are generally only used when we know all of the values
+    # it provides a `one_of` constraint, that only allows those values
+    constraints [one_of: [:open, :closed]]
+
+    # The status defaulting to open makes sense
+    default :open
+
+    # We also don't want status to ever be `nil`
+    allow_nil? false
+  end
   end
 end
